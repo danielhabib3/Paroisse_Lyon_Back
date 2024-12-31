d5df32ca0c4c40e4c97d094b416ba97cabdd52fe
danielhabib3 <danielhabib301@gmail.com>
2024-12-31 20:55:20 +0200

    api calls between front and back working

---
+++ src/main/java/com/example/Paroisse_Lyon_Back/ParoisseLyonBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.Paroisse_Lyon_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParoisseLyonBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParoisseLyonBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Paroisse_Lyon_Back/ParoisseLyonBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Paroisse_Lyon_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParoisseLyonBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
