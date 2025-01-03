ce086677b79b305da28f7c03d0f233b82390bee9
danielhabib3 <danielhabib301@gmail.com>
2025-01-03 21:58:10 +0200

    Changing API

--- src/main/java/com/example/Paroisse_Lyon_Back/API/API.java
+++ src/main/java/com/example/Paroisse_Lyon_Back/API/API.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:4200")
 public class API {
-    @GetMapping("/hello")
+    @GetMapping
     public String hello() {
         return "Backend is working!";
     }
